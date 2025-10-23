f9b8cd5986cac02fe2f8f5f89b872274980aec93
wildanfm19 <dracko.maaruftm@gmail.com>
2025-10-23 18:31:43 +0700

    Make generate email with webFlux to connect to google gemini API done

--- src/main/java/com/wildan/email_assistant/controller/EmailGeneratorController.java
+++ src/main/java/com/wildan/email_assistant/controller/EmailGeneratorController.java
@@ -1,16 +1,26 @@
 package com.wildan.email_assistant.controller;
 
 import com.wildan.email_assistant.dto.EmailRequest;
+import com.wildan.email_assistant.service.EmailGeneratorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
+@RequiredArgsConstructor
 public class EmailGeneratorController {
 
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
-        return ResponseEntity.ok("Test");
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
     }
+//
+//    @GetMapping("/test")
+//    public String testAPI(){
+//        return emailGeneratorService.testAPI();
+//    }
 }
--- src/main/java/com/wildan/email_assistant/service/EmailGeneratorService.java
+++ src/main/java/com/wildan/email_assistant/service/EmailGeneratorService.java
@@ -1,18 +1,69 @@
 package com.wildan.email_assistant.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wildan.email_assistant.dto.EmailRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.system.SystemProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class EmailGeneratorService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+//    public String testAPI(){
+//        return "url : " + geminiApiUrl + "\nkey : " + geminiApiKey;
+//    }
 
     public String generateEmailReply(EmailRequest emailRequest){
         // Buat prompt
         String prompt = buildPrompt(emailRequest);
         // Buat Request
-
+        Map<String ,Object> requestBody = Map.of(
+                "contents" , new Object[]{
+                        Map.of("parts", new Object[]{
+                            Map.of("text",prompt)
+                })
+                }
+        );
         // Lakuin Request ke geminiAPI dan dapetin response
-        // return responsenya
+        String response = webClient.post()
+                .uri(geminiApiUrl + "?key=" + geminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        // return extract response and return
+        return extractResponseContent(response);
+
+    }
+
+    private String extractResponseContent(String response) {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        } catch (Exception e){
+            return "Error processing request: " + e.getMessage();
+        }
     }
 
     private String buildPrompt(EmailRequest emailRequest) {
